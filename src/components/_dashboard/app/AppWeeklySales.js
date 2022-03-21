214b937f2cac94b4f689f72539fd0d2613b631be
GreenArmy18 <ofekcohen1803@gmail.com>
2022-03-21 18:27:31 +0200

    a

--- src/components/_dashboard/app/AppWeeklySales.js
+++ src/components/_dashboard/app/AppWeeklySales.js
@@ -42,7 +42,7 @@ export default function AppWeeklySales() {
       <IconWrapperStyle>
         <Icon icon={androidFilled} width={24} height={24} />
       </IconWrapperStyle>
-      <Typography variant="h3">{'455.34Mbps'}</Typography>
+      <Typography variant="h3">{455.34Mbps}</Typography>
       <Typography variant="subtitle2" sx={{ opacity: 0.72 }}>
         Best Download
       </Typography>
